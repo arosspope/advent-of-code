6fe60d4b122afacffae0cf75725e9037e4f20815
arosspope <andrew.pope456@gmail.com>
2018-12-10 10:44:53 +1100

    day1, part1 for aoc 2018

---
+++ aoc_2018/src/day1.rs
@@ -0,0 +1,16 @@
+//Day1: Chronal Calibration
+
+#[aoc_generator(day1)]
+pub fn input_frequencies(input: &str) -> Vec<i32> {
+    input.lines().map(|l| l.parse().unwrap()).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &[i32]) -> i32 {
+    input.iter().sum()
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &[i32]) -> i32 {
+    1
+}
---
+++ aoc_2018/src/main.rs
@@ -0,0 +1,7 @@
+extern crate aoc_2018;
+extern crate aoc_runner_derive;
+extern crate aoc_runner;
+
+use aoc_runner_derive::aoc_main;
+
+aoc_main! { lib = aoc_2018 }
