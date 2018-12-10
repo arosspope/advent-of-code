bbcff801d37828fe2dcb52e4f2ceac969ef99b46
arosspope <andrew.pope456@gmail.com>
2018-12-10 11:27:43 +1100

    addition of part1

--- aoc_2018/src/day1.rs
+++ aoc_2018/src/day1.rs
@@ -1,4 +1,5 @@
 //Day1: Chronal Calibration
+use std::collections::HashSet;
 
 #[aoc_generator(day1)]
 pub fn input_frequencies(input: &str) -> Vec<i32> {
@@ -12,7 +13,18 @@ pub fn part1(input: &[i32]) -> i32 {
 
 #[aoc(day1, part2)]
 pub fn part2(input: &[i32]) -> i32 {
-    1
+    let mut freq = 0;
+    let mut frequencies = HashSet::new();
+    frequencies.insert(freq);
+
+    for num in input.iter().cycle() {
+        freq += num;
+        if frequencies.contains(&freq) {
+            return freq;
+        }
+        frequencies.insert(freq);
+    }
+    unreachable!()
 }
 
 #[cfg(test)]
@@ -24,6 +36,6 @@ mod tests {
        assert_eq!(part2(&vec![1, -1]), 0);
        assert_eq!(part2(&vec![3, 3, 4, -2, -4]), 10);
        assert_eq!(part2(&vec![-6, 3, 8, 5, -6]), 5);
-       assert_eq!(part2(&vec![7, 7, -2, 7, -4]), 14);
+       assert_eq!(part2(&vec![7, 7, -2, -7, -4]), 14);
    }
 }
