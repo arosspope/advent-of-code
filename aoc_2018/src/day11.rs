c2b46695b9aa506348c43f7fca66c32ca63bfe6f
arosspope <andrew.pope456@gmail.com>
2018-12-14 07:08:20 +1100

    day11 part1

--- aoc_2018/src/day11.rs
+++ aoc_2018/src/day11.rs
@@ -1,12 +1,13 @@
 //Day 11: Chronal Charge
+
 use std::fmt;
 
 #[derive(Debug, Default, PartialEq, Eq)]
-pub struct PowerCell (usize, usize);
+pub struct PowerCell (usize, usize, usize); // Of the format (x, y, size)
 
 impl fmt::Display for PowerCell {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({},{})", self.0, self.1)
+        write!(f, "({},{},{})", self.0, self.1, self.2)
     }
 }
 
@@ -14,28 +15,28 @@ impl fmt::Display for PowerCell {
 pub fn part1(input: &str) -> PowerCell {
     let serial_num: isize = input.lines().next().unwrap().parse().unwrap();
 
-    let mut max = 0;
-    let mut max_fuel_cell: Option<PowerCell> = None;
-    for (x, y) in (1..298).zip(1..298) {
-        let fuel_square_power: isize = (0..2)
-            .zip(0..2)
+    let mut max: isize = 0;
+    let mut max_fuel_cell = PowerCell(0, 0, 3);
+
+    for (x, y) in iproduct!(1..299, 1..299) {
+        let fuel_square_power: isize = iproduct!(0..3, 0..3)
             .map(|(i, j)| cell_power(x + i, y + j, serial_num))
             .sum();
 
         if fuel_square_power > max {
             max = fuel_square_power;
-            max_fuel_cell = Some(PowerCell(x, y));
+            max_fuel_cell = PowerCell(x, y, 3);
         }
     }
 
-    max_fuel_cell.unwrap()
+    max_fuel_cell
 }
 
 fn cell_power(x: usize, y: usize, serial_num: isize) -> isize {
-    let rackid = x as isize + 10;
-    let mut power: isize = rackid * y as isize;
-    power += serial_num as isize;
-    power *= rackid;
+    let rackid = x + 10;
+    let mut power: isize = (rackid * y) as isize;
+    power += serial_num;
+    power *= rackid as isize;
     power = (power / 100) % 10;
     power - 5
 }
@@ -46,8 +47,12 @@ mod tests {
 
     #[test]
     fn sample1() {
-        assert_eq!(part1("57"), PowerCell(122,79));
-        assert_eq!(part1("39"), PowerCell(217,196));
-        assert_eq!(part1("71"), PowerCell(101,153));
+        assert_eq!(part1("18"), PowerCell(33,45,3));
+        assert_eq!(part1("42"), PowerCell(21,61,3));
+    }
+
+    #[test]
+    fn power_calc() {
+        assert_eq!(cell_power(3, 5, 8), 4);
     }
 }
--- aoc_2018/src/lib.rs
+++ aoc_2018/src/lib.rs
@@ -3,6 +3,9 @@ extern crate aoc_runner;
 #[macro_use]
 extern crate aoc_runner_derive;
 
+#[macro_use]
+extern crate itertools;
+
 pub mod day1;
 pub mod day2;
 pub mod day3;
