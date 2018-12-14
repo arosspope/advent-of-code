3ee8d4cc6cb3a4d438ebaffd9cdd8715cd6aa915
arosspope <andrew.pope456@gmail.com>
2018-12-14 12:32:20 +1100

    day 12 initial commit

---
+++ aoc_2018/src/day12.rs
@@ -0,0 +1,17 @@
+//Day 12: Subterranean Sustainability
+
+use std::fmt;
+
+#[aoc(day12, part1)]
+pub fn part1(input: &str) -> usize {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample1() {
+    }
+}
